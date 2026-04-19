a2fc708339b3d6881c72b7e2fc22e6963f86f502
emrott84 <emrott84@gmail.com>
2026-04-19 20:22:38 +0200

    Update service worker to include new asset paths

--- sw.js
+++ sw.js
@@ -1,19 +1,20 @@
 const CACHE_NAME = 'pilzlog-v4.5';
 const ASSETS = [
-  'index.html',
-  'manifest.json',
+  '/Pilz_Logbuch/',
+  '/Pilz_Logbuch/index.html',
+  '/Pilz_Logbuch/manifest.json',
+  '/Pilz_Logbuch/icon_192.png',
+  '/Pilz_Logbuch/icon_512.png',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
-// Installieren & Cachen
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Offline-Strategie: Erst Cache, dann Netzwerk
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
